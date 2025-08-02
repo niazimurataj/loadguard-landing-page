57619f2dd40455fa4f25a00d0eb295b84e4578b2
niazimurataj <niazi@murataj.com>
2025-08-01 23:49:38 -0700

    add AWS auth

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import 'mapbox-gl/dist/mapbox-gl.css';
+import { Amplify } from 'aws-amplify';
+import awsConfig from './data/aws-exports';
+
+Amplify.configure(awsConfig);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
